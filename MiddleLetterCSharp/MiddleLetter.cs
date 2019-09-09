02e906b56ed6cbc9999e719447a4085cdccde210
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-09-09 20:09:51 +0100

    Complete middle letter challenge

---
+++ MiddleLetterCSharp/MiddleLetter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MiddleLetterCSharp
+{
+    public class MiddleLetter
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public string Run(string input)
+        {
+            int halfLength = input.Length / 2;
+
+            if (input.Length % 2 == 1)
+            {
+                return "" + input[halfLength];
+            }
+            else
+            {
+                return "" + input[halfLength - 1] + input[halfLength];
+            }
+        }
+    }
+}
---
+++ MiddleLetterCSharpTests/UnitTest1.cs
@@ -0,0 +1,20 @@
+using MiddleLetterCSharp;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [TestCase("A", "A")]
+        [TestCase("AB", "AB")]
+        [TestCase("ABC", "B")]
+        [TestCase("ABCD", "BC")]
+
+        public void TestIOFormat(string input, string expect)
+        {
+            MiddleLetter middle = new MiddleLetter();
+            string result = middle.Run(input);
+            Assert.AreEqual(result, expect);
+        }
+    }
+}
